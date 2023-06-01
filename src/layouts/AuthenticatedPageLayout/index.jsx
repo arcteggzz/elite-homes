860a56f6db5a328df90e78a5da138940d9930fd7
Oghenetega Esedere <arcteggzz@gmail.com>
2023-06-01 18:46:39 +0100

    feat: implemented Navbar and home page navigation

--- src/layouts/AuthenticatedPageLayout/index.jsx
+++ src/layouts/AuthenticatedPageLayout/index.jsx
@@ -1,30 +1,58 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import AuthenticatedPageHeader from "./Components/AuthenticatedPageHeader";
 import AuthenticatedPageNavigation from "./Components/AuthenticatedPageNavigation";
 import styles from "./AuthenticatedPageLayout.module.scss";
+import { useSelector } from "react-redux";
+import { selectCurrentAccessToken } from "../../redux/features/auth/authSlice";
 
 export default function AuthenticatedPageLayout() {
+  const location = useLocation();
+  const routeName = location.pathname.substring(1, location.pathname.length);
+  const currentAccessToken = useSelector(selectCurrentAccessToken);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: -200,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, []);
+
+  // if (token) {
+  //   return (
+
+  //   )
+  // }
+
   return (
     <>
-      <div className={styles.AuthenticatedPageLayout}>
-        <div className={styles.main_container}>
-          <div className={styles.Navbar_container}>
-            <div className={styles.Navbar_Child_Container}>
-              <Navbar />
+      {currentAccessToken ? (
+        <div className={styles.AuthenticatedPageLayout}>
+          <div className={styles.main_container}>
+            <div className={styles.Navbar_container}>
+              <div className={styles.Navbar_Child_Container}>
+                <Navbar
+                  isAuthenticated={currentAccessToken ? true : false}
+                  isHomepageNavbar={routeName === "home"}
+                />
+              </div>
             </div>
-          </div>
-          <div className={styles.Outlet_container}>
-            <div className={styles.Outlet_Child_Container}>
-              <AuthenticatedPageHeader />
-              <AuthenticatedPageNavigation />
-              <Outlet />
+            <div className={styles.Outlet_container}>
+              <div className={styles.Outlet_Child_Container}>
+                <AuthenticatedPageHeader />
+                <AuthenticatedPageNavigation />
+                <Outlet />
+              </div>
             </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
-      </div>
+      ) : (
+        <Navigate to={"/login"} state={{ from: location }} replace />
+      )}
     </>
   );
 }
--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import propertySlice from "../features/propertySlice/propertySlice";
+import propertySlice from "../features/property/propertySlice";
+import authReducer from "../features/auth/authSlice";
 
 const store = configureStore({
   reducer: {
     propertySlice,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
   devTools: true,
