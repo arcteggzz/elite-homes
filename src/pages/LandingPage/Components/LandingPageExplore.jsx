f3c3be17cd0fe890577d38856067a3daeafe7330
Oghenetega Esedere <arcteggzz@gmail.com>
2023-06-01 11:30:00 +0100

    feat: demoed landing page and repo contents

---
+++ src/pages/LandingPage/Components/LandingPageExplore.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/LandingPageExplore.module.scss";
+
+const LandingPageExplore = () => {
+  return (
+    <>
+      <section className={styles.LandingPageExplore}>
+        <button>Search</button>
+      </section>
+    </>
+  );
+};
+
+export default LandingPageExplore;
