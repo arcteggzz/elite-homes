499c09dd8070116d043aaa8baa99f503b21d37ad
Oghenetega Esedere <arcteggzz@gmail.com>
2023-06-07 17:20:51 +0100

    feat: completed properties offers page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,36 +1,22 @@
 import styles from "./LoginPage.module.scss";
-import image from "./LoginAssets/loginimage.png";
+import login_image from "./LoginAssets/loginimage.png";
 import AnimatedFadeInPage from "../../utils/AnimatedFadeInPage";
+import { Link } from "react-router-dom";
+import elite_homes_black_icon from "../../components/Navbar/images/elite_homes_black_icon.png";
 
 const LoginPage = () => {
   return (
     <>
       <AnimatedFadeInPage>
         <main className={styles.LoginPage}>
-          <h1>Elite Homes</h1>
-          <div className={styles.LoginContentContainer}>
-          <div className={styles.LoginContent}>
-            <img src={image} alt="Login" className={styles.LoginImage} />
-            <div className={styles.LoginContentB}>
-              <h2>
-                <span className={styles.Hello}>Hello!</span> Welcome back
-              </h2>
-              <p className={styles.LoginText}>Log in with the data you entered during sign up</p>
-              <form className={styles.LoginForm}>
-                <input type="email" placeholder="Email" className={styles.LoginInput} />
-                <input type="password" placeholder="Password" className={styles.LoginInput} />
-                <div className={styles.LoginRememberMe} >
-                  <div className={styles.LoginCheckBox} >
-                    <input type="checkbox" />
-                    <p>Remember me</p>
-                  </div>
-                  <p className={styles.FogetPassword}>Forgot Password?</p>
-                </div>
-                <input type="submit" value="Log In" className={styles.LoginButton}/>
-              </form>
-              <p className={styles.LoginSignUpText}>Don't have an account? <span>Sign Up</span></p>
-            </div>
-          </div>
+          <nav>
+            <Link className={styles.left} to={"/"}>
+              <img src={elite_homes_black_icon} alt="elite homes icon" />
+            </Link>
+          </nav>
+          <div>
+            <img src={login_image} alt="" />
+            <form></form>
           </div>
         </main>
       </AnimatedFadeInPage>
--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,33 +1,22 @@
 import styles from "./SignUpPage.module.scss";
-import image from "./SignUpAssets/signUpImage.png";
+// import image from "./SignUpAssets/signUpImage.png";
 import AnimatedFadeInPage from "../../utils/AnimatedFadeInPage";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <>
       <AnimatedFadeInPage>
-        <main className={styles.SignUpPage}>
-          <h1>Elite Homes</h1>
-         <div className={styles.SignUpContentContainer}>
-         <div className={styles.SignUpContent}>
-            <img src={image} alt="Login" className={styles.SignUpImage} />
-            <div className={styles.SignUpContentB}>
-              <p className={styles.welcome}>Welcome to</p>
-              <h2>
-                Elite Homes
-              </h2>
-              <p className={styles.SignUpText}>Let's get you started, input your details below.</p>
-              <form className={styles.SignUpForm}>
-                <input type="email" placeholder="Email" className={styles.SignUpInput} />
-                <input type="password" placeholder="Password" className={styles.SignUpInput} />
-                <input type="password" placeholder="Confirm Password" className={styles.SignUpInput} />
-                <input type="tel" placeholder="Phone number" className={styles.SignUpInput} />
-                <input type="submit" value="Continue" className={styles.SignUpButton}/>
-              </form>
-              <p className={styles.SignUpLoginText}>Already have an account? <span>Log In</span></p>
-            </div>
+        <main className={styles.LoginPage}>
+          <nav>
+            <Link className={styles.left} to={"/"}>
+              {/* <img src={elite_homes_black_icon} alt="elite homes icon" /> */}
+            </Link>
+          </nav>
+          <div>
+            {/* <img src={login_image} alt="" /> */}
+            <form></form>
           </div>
-         </div>
         </main>
       </AnimatedFadeInPage>
     </>
